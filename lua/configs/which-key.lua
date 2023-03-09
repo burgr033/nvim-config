e54332ed342390da27807a80b8b1f1a00c426468
burgr <simon@burgr.io>
2023-03-09 13:12:19 +0100

    added operator trigger for whichkey, because i forget stuff

--- lua/configs/which-key.lua
+++ lua/configs/which-key.lua
@@ -1,11 +1,10 @@
 require("which-key").setup(astronvim.user_plugin_opts("plugins.which-key", {
   plugins = {
     spelling = { enabled = true },
-    presets = { operators = false },
   },
   window = {
     border = "rounded",
-    padding = { 2, 2, 2, 2 },
+    padding = { 1, 1, 1, 1 },
   },
   disable = { filetypes = { "TelescopePrompt" } },
 }))
