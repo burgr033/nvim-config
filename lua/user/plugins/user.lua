51d03ae5953fce151689bd44a3e58834ea8d3c0e
SB <simon@burgr.io>
2023-06-19 17:13:44 +0200

    added vimtex support

--- lua/user/plugins/user.lua
+++ lua/user/plugins/user.lua
@@ -11,24 +11,64 @@ return {
 	-- },
 	{
 		"mfussenegger/nvim-dap",
-		enabled = true
+		enabled = true,
 	},
 	{
 		"goolord/alpha-nvim",
 		dependencies = { "cigh033/alpha-nvim-bofh-excuse" },
-		cmd = "Alpha"
-	},	
-	
+		cmd = "Alpha",
+	},
+	{
+		"lervag/vimtex",
+		ft = { "tex", "bib" },
+		opts = { patterns = { "*.tex" } },
+		config = function(_, opts)
+			vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+				pattern = opts.patterns,
+				callback = function()
+					vim.cmd([[VimtexCompile]])
+				end,
+			})
+			vim.g.vimtex_compiler_latexmk = {
+				build_dir = ".out",
+				options = {
+					"-shell-escape",
+					"-verbose",
+					"-file-line-error",
+					"-interaction=nonstopmode",
+					"-synctex=1",
+				},
+			}
+			vim.g.vimtex_view_general_viewer = "SumatraPDF"
+			vim.g.vimtex_view_general_options = "-reuse-instance -forward-search @tex @line @pdf"
+			vim.g.vimtex_fold_enabled = true
+			vim.g.vimtex_syntax_conceal = {
+				accents = 1,
+				ligatures = 1,
+				cites = 1,
+				fancy = 1,
+				spacing = 0, -- default: 1
+				greek = 1,
+				math_bounds = 1,
+				math_delimiters = 1,
+				math_fracs = 1,
+				math_super_sub = 1,
+				math_symbols = 1,
+				sections = 0,
+				styles = 1,
+			}
+		end,
+	},
 	{
 		"CRAG666/code_runner.nvim",
-		cmd = "RunCode"
+		cmd = "RunCode",
 	},
 	{
 		"mfussenegger/nvim-jdtls",
-		ft = "java"
+		ft = "java",
 	},
 	{
 		"natecraddock/workspaces.nvim",
-		cmd = "Telescope workspaces"
+		cmd = "Telescope workspaces",
 	},
 }
