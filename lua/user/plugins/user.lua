f5e7b91f91903789a9523d3d9acafef67df22348
sb <simon@burgr.io>
2023-06-24 22:27:52 +0200

    fixed if windows

--- lua/user/plugins/user.lua
+++ lua/user/plugins/user.lua
@@ -39,11 +39,11 @@ return {
 					"-synctex=1",
 				},
 			}
-			if vim.fn.has("win32") then
+			if vim.fn.has("win32") == 1 then
 				vim.g.vimtex_view_general_viewer = "SumatraPDF"
 				vim.g.vimtex_view_general_options = "-reuse-instance -forward-search @tex @line @pdf"
 			end
-			if vim.fn.has("linux") then
+			if vim.fn.has("linux") == 1 then
 				vim.g.vimtex_view_method = "zathura"
 			end
 			vim.g.vimtex_fold_enabled = true
