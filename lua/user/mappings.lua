b3ec835a9854484da9a3218db3d7bb5ade849643
sb <simon@burgr.io>
2023-06-24 22:53:41 +0200

    bumped

--- lua/user/mappings.lua
+++ lua/user/mappings.lua
@@ -47,7 +47,6 @@ return {
     -- quick save
     -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
   },
-  i = {},
   t = {
     -- setting a mapping to false will disable it
     -- ["<esc>"] = false,
--- lua/user/options.lua
+++ lua/user/options.lua
@@ -7,7 +7,7 @@ return {
     spell = false, -- sets vim.opt.spell
     signcolumn = "auto", -- sets vim.opt.signcolumn to auto
     wrap = false, -- sets vim.opt.wrap
-	cmdheight = 1,
+    cmdheight = 1,
   },
   g = {
     mapleader = " ", -- sets vim.g.mapleader
