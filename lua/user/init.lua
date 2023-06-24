0c6648e186afdefbb5d16f7a6087a702fe4fb5ee
sb <simon@burgr.io>
2023-06-24 22:53:41 +0200

    bumped

--- lua/user/init.lua
+++ lua/user/init.lua
@@ -20,7 +20,7 @@ return {
   colorscheme = "astrodark",
   -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
   diagnostics = {
-    virtual_text = false,
+    virtual_text = true,
     update_in_insert = true,
     underline = true,
   },
