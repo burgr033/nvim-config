6e94f554a129d05d5d5524b9572a592f625a34b8
SB <simon@burgr.io>
2023-06-27 10:12:54 +0200

    changed config of alterantive cmp mapping to user config

--- lua/user/init.lua
+++ lua/user/init.lua
@@ -1,16 +1,16 @@
 return {
   -- Configure AstroNvim updates
   updater = {
-    remote = "origin", -- remote to use
-    channel = "stable", -- "stable" or "nightly"
-    version = "latest", -- "latest", tag name, or regex search like "v1.*" to only do updates before v2 (STABLE ONLY)
-    branch = "nightly", -- branch name (NIGHTLY ONLY)
-    commit = nil, -- commit hash (NIGHTLY ONLY)
-    pin_plugins = nil, -- nil, true, false (nil will pin plugins on stable only)
-    skip_prompts = false, -- skip prompts about breaking changes
+    remote = "origin",     -- remote to use
+    channel = "stable",    -- "stable" or "nightly"
+    version = "latest",    -- "latest", tag name, or regex search like "v1.*" to only do updates before v2 (STABLE ONLY)
+    branch = "nightly",    -- branch name (NIGHTLY ONLY)
+    commit = nil,          -- commit hash (NIGHTLY ONLY)
+    pin_plugins = nil,     -- nil, true, false (nil will pin plugins on stable only)
+    skip_prompts = false,  -- skip prompts about breaking changes
     show_changelog = true, -- show the changelog after performing an update
-    auto_quit = false, -- automatically quit the current session after a successful update
-    remotes = { -- easily add new remotes to track
+    auto_quit = false,     -- automatically quit the current session after a successful update
+    remotes = {            -- easily add new remotes to track
       --   ["remote_name"] = "https://remote_url.come/repo.git", -- full remote url
       --   ["remote2"] = "github_user/repo", -- GitHub user/repo shortcut,
       --   ["remote3"] = "github_user", -- GitHub user assume AstroNvim fork
@@ -39,8 +39,8 @@ return {
 
       local cursor_pos = vim.api.nvim_win_get_cursor(0)
       if
-        (cursor_pos[1] ~= vim.b.diagnostics_pos[1] or cursor_pos[2] ~= vim.b.diagnostics_pos[2])
-        and #vim.diagnostic.get() > 0
+          (cursor_pos[1] ~= vim.b.diagnostics_pos[1] or cursor_pos[2] ~= vim.b.diagnostics_pos[2])
+          and #vim.diagnostic.get() > 0
       then
         vim.diagnostic.open_float(nil, float_opts)
       end
@@ -52,7 +52,7 @@ return {
     formatting = {
       -- control auto formatting on save
       format_on_save = {
-        enabled = true, -- enable or disable format on save globally
+        enabled = true,     -- enable or disable format on save globally
         allow_filetypes = { -- enable format on save for specified filetypes only
           -- "go",
         },
@@ -95,25 +95,12 @@ return {
   -- augroups/autocommands and custom filetypes also this just pure lua so
   -- anything that doesn't fit in the normal config locations above can go here
   polish = function()
-    -- Set up custom filetypes
-    -- vim.filetype.add {
-    --   extension = {
-    --     foo = "fooscript",
-    --   },
-    --   filename = {
-    --     ["Foofile"] = "fooscript",
-    --   },
-    --   pattern = {
-    --     ["~/%.config/foo/.*"] = "fooscript",
-    --   },
-    -- }
-    -- see https://github.com/nvim-treesitter/nvim-treesitter/issues/1985
-    -- some Treesitter parsers dont work on windows if they were compiled with gcc or something else. Zig is much easier to use under windows
     if vim.fn.has "win32" == 1 then
       require("nvim-treesitter.install").compilers = { "zig" }
       local powershell_options = {
         shell = vim.fn.executable "pwsh" == 1 and "pwsh" or "powershell",
-        shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;",
+        shellcmdflag =
+        "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;",
         shellredir = "-RedirectStandardOutput %s -NoNewWindow -Wait",
         shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode",
         shellquote = "",
@@ -124,11 +111,12 @@ return {
         vim.opt[option] = value
       end
     end
+
     local telescope = require "telescope"
     local utils = require "astronvim.utils"
     utils.conditional_func(telescope.load_extension, utils.is_available "workspaces.nvim", "workspaces")
-    require("ltex-client").setup()
 
+    require("ltex-client").setup()
     require("lspconfig").ltex.setup {
       settings = {
         ltex = {
@@ -136,15 +124,12 @@ return {
         },
       },
     }
-
-    require("mason").setup {
-      log_level = vim.log.levels.DEBUG,
-    }
     vim.api.nvim_create_user_command(
       "InitDefaultMason",
       "MasonInstall codelldb gitlint intelephense json-lsp jsonlint lemminx lua-language-server php-debug-adapter phpstan python-lsp-server rust-analyzer rustfmt selene xmlformatter yaml-language-server yamlfmt yamllint",
       { desc = "Init my default mason packages" }
     )
+
     vim.api.nvim_create_user_command(
       "InitDefaultTreeSitter",
       "TSInstall! gitcommit html javascript lua php python rust vim bash dockerfile markdown make json toml yaml",
