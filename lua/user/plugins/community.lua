e6ba19d1de1704d398eca585ce401cf5d5abbfb1
SB <simon@burgr.io>
2023-07-21 12:38:03 +0200

    changed some tex stuff and changed colorscheme

--- lua/user/plugins/community.lua
+++ lua/user/plugins/community.lua
@@ -1,17 +1,18 @@
 return {
-	-- Add the community repository of plugin specifications
-	"AstroNvim/astrocommunity",
-	-- example of imporing a plugin, comment out to use it or add your own
-	-- available plugins can be found at https://github.com/AstroNvim/astrocommunity
+  -- Add the community repository of plugin specifications
+  "AstroNvim/astrocommunity",
+  -- example of imporing a plugin, comment out to use it or add your own
+  -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
-	{ import = "astrocommunity.bars-and-lines.heirline-vscode-winbar" },
-	{ import = "astrocommunity.bars-and-lines.heirline-mode-text-statusline" },
-	-- { import = "astrocommunity.pack.json" },
-	{ import = "astrocommunity.pack.rust" },
-	-- { import = "astrocommunity.pack.java" },
-	-- { import = "astrocommunity.pack.bash" },
-	-- { import = "astrocommunity.pack.yaml" },
-	-- { import = "astrocommunity.pack.toml" },
-	-- { import = "astrocommunity.pack.python" },
-	-- { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.bars-and-lines.heirline-vscode-winbar" },
+  { import = "astrocommunity.bars-and-lines.heirline-mode-text-statusline" },
+  -- { import = "astrocommunity.pack.json" },
+  { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.colorscheme.catppuccin" },
+  -- { import = "astrocommunity.pack.java" },
+  -- { import = "astrocommunity.pack.bash" },
+  -- { import = "astrocommunity.pack.yaml" },
+  -- { import = "astrocommunity.pack.toml" },
+  -- { import = "astrocommunity.pack.python" },
+  -- { import = "astrocommunity.pack.lua" },
 }
