94efd22ffa41dd88c73e54f9e0f88132290b6a3c
SB <simon@burgr.io>
2023-09-21 12:34:58 +0200

    added cpp

--- lua/user/plugins/core.lua
+++ lua/user/plugins/core.lua
@@ -52,6 +52,9 @@ return {
         mode = "toggleterm",
         before_run_filetype = function() vim.cmd ":w" end,
         filetype = {
+          cpp = {
+            "cd $dir && clang++ $fileName --target=x86_64-w64-windows-gnu -o $fileNameWithoutExt && $dir/$fileNameWithoutExt",
+          },
           ps1 = {
             "cd $dir && powershell -file $fileName",
           },
