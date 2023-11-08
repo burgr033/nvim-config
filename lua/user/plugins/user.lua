5d39227877b00b0480bf4aff001df9bc9dfa919c
sb <simon@burgr.io>
2023-11-08 13:02:19 +0100

    changed to everforest

--- lua/user/plugins/user.lua
+++ lua/user/plugins/user.lua
@@ -103,6 +103,21 @@ return {
     end,
   },
   {
+    "neanias/everforest-nvim",
+    version = false,
+    lazy = false,
+    priority = 1000, -- make sure to load this before all the other start plugins
+    -- Optional; default configuration will be used if setup isn't called.
+    config = function()
+      require("everforest").setup {
+        -- Your config here
+        background = "hard",
+        dim_inactive_windows = true,
+        ui_contrast = "high",
+      }
+    end,
+  },
+  {
     "akinsho/toggleterm.nvim",
     opts = {
       direction = "horizontal",
