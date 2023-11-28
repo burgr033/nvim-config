bc3ef9c160d9793827e6a5f45f4cb5349932c30e
sb <simon@burgr.io>
2023-11-28 12:05:31 +0100

    bumped

--- lua/user/init.lua
+++ lua/user/init.lua
@@ -73,8 +73,9 @@ return {
         },
       },
       disabled = { -- disable formatting capabilities for the listed language servers
-        -- "sumneko_lua",
-      },
+        -- disable lua_ls formatting capability if you want to use StyLua to format your lua code
+        -- "lua_ls",
+        },
       timeout_ms = 1000, -- default format timeout
       -- filter = function(client) -- fully override the default formatting function
       --   return true
@@ -82,12 +83,7 @@ return {
     },
     -- enable servers that you already have installed without mason
     servers = {
-      --  "pyright",
-      --  ["server-settings"] = {
-      --    ltex = {
-      --      language = "de-DE",
-      --    },
-      --  },
+      --  "pyright"
     },
   },
   -- Configure require("lazy").setup() options
