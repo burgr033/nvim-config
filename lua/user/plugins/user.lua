08419b6be1a4a6a504cc0c6ed99f50b884cd5481
sb <simon@burgr.io>
2024-03-17 09:17:33 +0100

    added nvim surround

--- lua/user/plugins/user.lua
+++ lua/user/plugins/user.lua
@@ -1,9 +1,19 @@
 return {
   {
     "burgr033/mf-runner.nvim",
-    cmd = { "MFROpen", "MFRRun", "MFRCreate" },
+    cmd = { "MFROpen", "MFRRun", "MFREdit" },
     dependencies = { "nvim-telescope/telescope.nvim", "akinsho/toggleterm.nvim" },
     opts = {},
+  },
+  {
+    "kylechui/nvim-surround",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+      require("nvim-surround").setup {
+        -- Configuration here, or leave empty to use defaults
+      }
+    end,
   },
   {
     "dgagn/diagflow.nvim",
