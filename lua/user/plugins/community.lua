b712262d27b248509a4b224ea6b42d40a5c4eb82
sb <simon@burgr.io>
2024-04-02 16:29:52 +0200

    fixate commit for astrocommunity until i get time to redo my config because the stopped distributing the stuff i use

--- lua/user/plugins/community.lua
+++ lua/user/plugins/community.lua
@@ -1,5 +1,5 @@
 return {
-  "AstroNvim/astrocommunity",
+  { "AstroNvim/astrocommunity", commit = "f0a6043" },
   { import = "astrocommunity.bars-and-lines.heirline-vscode-winbar" },
   { import = "astrocommunity.bars-and-lines.heirline-mode-text-statusline" },
   { import = "astrocommunity.colorscheme.onedarkpro-nvim" },
