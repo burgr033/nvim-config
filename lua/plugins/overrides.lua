4963b367c2d4bfee0a55fa5321547b14ef3d3567
sb <simon@burgr.io>
2024-04-05 13:41:36 +0200

    added snippets back in

---
+++ lua/plugins/overrides.lua
@@ -0,0 +1,46 @@
+---@type LazySpec
+return {
+  {
+    "goolord/alpha-nvim",
+    dependencies = { "burgr033/alpha-nvim-bofh-excuse" },
+    opts = function(_, opts)
+      opts.section.header.val = {
+        "██████████████████████████████████████████████████████████████████████████",
+        "██                                                                      ██",
+        "██  IF YOU WANT IMMEDIATE FEEDBACK, ALWAYS MAKE CHANGES IN PRODUCTION.  ██",
+        "██                                                                      ██",
+        "██████████████████████████████████████████████████████████████████████████",
+      }
+      opts.section.header.opts.hl = "DashboardHeader"
+      -- local button = require "astroui".alpha
+      local get_icon = require("astroui").get_icon
+      local button = require("alpha.themes.dashboard").button
+      opts.section.buttons.val = {
+        button("LDR n  ", get_icon("FileNew", 2, true) .. "New File  "),
+        button("LDR W o", get_icon("FolderOpen", 2, true) .. "Workspaces  "),
+        button("LDR f o", get_icon("DefaultFile", 2, true) .. "Recents  "),
+        button("LDR f f", get_icon("Search", 2, true) .. "Find File  "),
+        button("LDR f w", get_icon("WordFile", 2, true) .. "Find Word  "),
+        button("LDR S l", get_icon("Refresh", 2, true) .. "Last Session  "),
+      }
+      opts.section.header.opts.hl = "DashboardFooter"
+      local excuse = require "alpha.excuse"
+      opts.section.footer.val = excuse()
+      opts.config.layout[1].val = vim.fn.max { 2, vim.fn.floor(vim.fn.winheight(0) * 0.2) }
+      opts.config.layout[3].val = 5
+      opts.config.opts.noautocmd = true
+      return opts
+    end,
+    config = function(_, opts) require("alpha").setup(opts.config) end,
+  },
+  -- You can also easily customize additional setup of plugins that is outside of the plugin's setup call
+  {
+    "L3MON4D3/LuaSnip",
+    config = function(plugin, opts)
+      require "astronvim.plugins.configs.luasnip"(plugin, opts)
+      require("luasnip.loaders.from_snipmate").lazy_load {
+        paths = { vim.fn.stdpath "config" .. "/lua/snippets" },
+      }
+    end,
+  },
+}
