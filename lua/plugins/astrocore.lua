c24e59f6fd035515861ca5ad866aca947b980639
sb <simon@burgr.io>
2024-04-05 13:04:53 +0200

    asdf

--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -30,7 +30,7 @@ return {
         spell = false, -- sets vim.opt.spell
         signcolumn = "auto", -- sets vim.opt.signcolumn to auto
         wrap = false, -- sets vim.opt.wrap
-        cmdheight = 0,
+        cmdheight = 1,
       },
       g = { -- vim.g.<key>
         -- configure global vim variables (vim.g)
