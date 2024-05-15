27c5c61a765b1e50654aeb0ec85b12628fbdb3eb
sb <simon@burgr.io>
2024-05-15 09:18:11 +0200

    removed mini.files

--- lua/community.lua
+++ lua/community.lua
@@ -5,6 +5,5 @@ return {
   { import = "astrocommunity.colorscheme.onedarkpro-nvim" },
   { import = "astrocommunity.recipes.heirline-mode-text-statusline" },
   { import = "astrocommunity.recipes.heirline-vscode-winbar" },
-  { import = "astrocommunity.file-explorer.mini-files" },
   { import = "astrocommunity.diagnostics.trouble-nvim" },
 }
