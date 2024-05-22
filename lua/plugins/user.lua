c229555bd5566b71b05ca1ef823cfdc1bc26ab43
sb <simon@burgr.io>
2024-05-22 23:31:29 +0200

    removed codeium

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -136,15 +136,4 @@ return {
 
     opts = {},
   },
-  {
-    "natecraddock/workspaces.nvim",
-    cmd = { "WorkspacesAdd", "WorkspacesList", "WorkspacesOpen" },
-    config = function()
-      require("workspaces").setup {
-        hooks = {
-          open = { "Neotree toggle" },
-        },
-      }
-    end,
-  },
 }
