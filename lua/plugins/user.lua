54a6e526b8fb85b062746928feacf351eec1676a
sb <simon@burgr.io>
2024-05-27 12:27:34 +0200

    remove codeium

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -15,22 +15,6 @@ return {
     end,
   },
   {
-    "Exafunction/codeium.vim",
-    event = "User AstroFile",
-    config = function()
-      vim.g.codeium_no_map_tab = true
-      vim.g.codeium_disable_bindings = true
-      vim.keymap.set("i", "<C-g>", function() return vim.fn["codeium#Accept"]() end, { expr = true })
-      vim.keymap.set("n", "<Leader>;", function()
-        if vim.g.codeium_enabled == true then
-          vim.cmd "CodeiumDisable"
-        else
-          vim.cmd "CodeiumEnable"
-        end
-      end, { noremap = true, desc = "Toggle Codeium" })
-    end,
-  },
-  {
     "zbirenbaum/copilot.lua",
     opts = {
       suggestion = { enabled = false },
