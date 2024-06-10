1bbc9ae135ab477743258c1a6dd42bd4df628eca
sb <simon@burgr.io>
2024-06-10 21:11:45 +0200

    asdf

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -15,32 +15,26 @@ return {
     end,
   },
   {
+    "tris203/precognition.nvim",
+    event = "VeryLazy",
+    config = {},
+  },
+  {
     "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    event = "User AstroFile",
     opts = {
-      suggestion = { enabled = false },
-      panel = { enabled = false },
+      suggestion = {
+        auto_trigger = true,
+        debounce = 150,
+        keymap = {
+          accept = "<C-g>",
+          next = "<C-.>",
+          prev = "<C-,",
+        },
+      },
     },
   },
-  { "zbirenbaum/copilot-cmp", opts = {}, dependencies = { "zbirenbaum/copilot.lua" } },
-  {
-    "hrsh7th/nvim-cmp",
-    dependencies = { "zbirenbaum/copilot-cmp" },
-    opts = function(_, opts)
-      -- Inject copilot into cmp sources, with high priority
-      table.insert(opts.sources, 1, {
-        name = "copilot",
-        group_index = 1,
-        priority = 10000,
-      })
-    end,
-  },
-  {
-    "onsails/lspkind.nvim",
-    optional = true,
-    -- Adds icon for copilot using lspkind
-    opts = function(_, opts) opts.symbol_map.Copilot = "" end,
-  },
-
   {
     "burgr033/mf-runner.nvim",
     cmd = { "MFROpen", "MFRRun", "MFREdit" },
