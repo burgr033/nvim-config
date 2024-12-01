0f255e86d974ffaa720b850aa89862b3639b6db7
sb <burgr@posteo.de>
2024-12-01 22:44:06 +0100

    added tokyonight

--- lua/community.lua
+++ lua/community.lua
@@ -13,6 +13,7 @@ return {
   { import = "astrocommunity.file-explorer.oil-nvim" },
   { import = "astrocommunity.editing-support.neogen" },
   { import = "astrocommunity.colorscheme.onedarkpro-nvim" },
+  { import = "astrocommunity.colorscheme.tokyonight-nvim" },
   { import = "astrocommunity.recipes.heirline-mode-text-statusline" },
   { import = "astrocommunity.recipes.heirline-vscode-winbar" },
   { import = "astrocommunity.diagnostics.trouble-nvim" },
