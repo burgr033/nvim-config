ab0b228b1f0142e37af61f35886736a3ff76f29d
Simon Burger <Simon.Burger@th-ab.de>
2025-02-26 12:03:14 +0100

    modified:   lua/polish.lua added OSC52

--- lua/polish.lua
+++ lua/polish.lua
@@ -22,3 +22,19 @@ if vim.fn.has "win32" == 1 then
     vim.opt[option] = value
   end
 end
+
+if vim.env.SSH_TTY then
+  vim.o.clipboard = "unnamedplus"
+  local osc52 = require "vim.ui.clipboard.osc52"
+  vim.g.clipboard = {
+    name = "OSC 52",
+    copy = {
+      ["+"] = osc52.copy "+",
+      ["*"] = osc52.copy "*",
+    },
+    paste = {
+      ["+"] = osc52.paste "+",
+      ["*"] = osc52.paste "*",
+    },
+  }
+end
