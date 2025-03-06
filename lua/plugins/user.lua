6e81d47fa9ba97e3b73f50cfbf14ad388f8d09d8
sb <burgr@posteo.de>
2025-03-06 10:12:41 +0100

    init v5

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -1,26 +1,16 @@
 ---@type LazySpec
 return {
   {
-    "FerretDetective/oil-git-signs.nvim",
+    "refractalize/oil-git-status.nvim",
     ft = "oil",
-    opts = {},
-  },
-  {
-    "leoluz/nvim-dap-go",
-    ft = "go",
-    lazy = true,
-    opts = {
-      require("dap-go").setup {
-        delve = {
-          detached = false,
-        },
-      },
+    dependencies = {
+      "stevearc/oil.nvim",
     },
+    config = true,
   },
   {
     "burgr033/mf-runner.nvim",
     cmd = { "MFROpen", "MFRRun", "MFREdit" },
-    dependencies = { "nvim-telescope/telescope.nvim", "akinsho/toggleterm.nvim" },
     opts = {},
   },
   {
@@ -30,26 +20,6 @@ return {
       vim.g.tex_flavor = "latex"
       vim.opt.wrap = true
       require("ltex-client").setup {}
-    end,
-  },
-  {
-    "kosayoda/nvim-lightbulb",
-    event = "LspAttach",
-
-    opts = {},
-  },
-  {
-    "natecraddock/workspaces.nvim",
-    cmd = { "WorkspacesAdd", "WorkspacesList", "WorkspacesOpen" },
-    config = function()
-      require("workspaces").setup {
-        hooks = {
-          open = { "Oil" },
-        },
-      }
-      local telescope = require "telescope"
-      local utils = require "astrocore"
-      utils.conditional_func(telescope.load_extension, utils.is_available "workspaces.nvim", "workspaces")
     end,
   },
 }
